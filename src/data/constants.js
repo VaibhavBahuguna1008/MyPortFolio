b3e5ac5a047e8e9996b4b0044211f8172ea5bf69
Vaibhav Bahuguna <vaibhavbahuguna423@gmail.com>
2025-01-07 21:44:52 +0530

    Added Files

---
+++ src/data/constants.js
@@ -0,0 +1,398 @@
+export const Bio = {
+  name: "Vaibhav Bahuguna",
+  roles: [
+    "MERN Stack Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/VaibhavBahuguna1008",
+  resume:
+    "https://docs.google.com/document/d/1qJrn6Rt4kRKzJwTiMvWctAkHijuOGBq8y49RZ0KWZsI/edit?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/vaibhav-bahuguna-733a9727a/",
+  insta: "https://www.instagram.com/vaibhavbahuguna1008/ ",
+  LeetCode: "https://leetcode.com/u/vaibhav423/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind",
+        image:
+          "https://pbs.twimg.com/profile_images/1730334391501488129/G0R0sjHH_400x400.jpg",
+      },
+
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png",
+      },
+      {
+        name: "REST",
+        image: "https://www.sovereignconsult.com/wp-content/uploads/2020/03/RestApi.png",
+      },
+
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "DevOps",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+
+    ],
+  },
+ 
+  {
+    title: "Others",
+    skills: [
+
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "Vercel",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkAuKrVgOa4BJxUnH4gdJ5TV0m2IFEMjLJ2g&s",
+      },
+      {
+        name: "Render",
+        image:
+          "https://pbs.twimg.com/profile_images/1735429515541938176/zOO1N7Su_400x400.jpg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+
+    ],
+  },
+];
+
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://play-lh.googleusercontent.com/3AIeSG0MgjUjA-oOYOoSCu802l5Lv5D96brKajRdf01r2IavfOhcmrs_JhkSfhndyA=w600-h300-pc0xffffff-pd",
+    school: "Guru Tegh Bahadur Institute of Technology",
+    date: "Dec 2022 - Aug 2026(Expected)",
+    grade: "8.75 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Information and Technology Engineering at Guru Tegh Bahadur Institute of Technology, New Delhi. I have completed 4 semesters and have a avg. CGPA of 8.75. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Information & Technology and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPbZxudH19LmrrPjG2R1Mx80VJAMOCvCNx-A&s",
+    school: "Ryan International School, Delhi",
+    date: "Apr 2021 - Apr 2022",
+    grade: "91.8%",
+    desc: "I completed my class 12 high school education at Ryan International School, Delhi, where I studied PCM with Computer Science.",
+    degree: "CBSE(XII), PCM with Computer Science",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPbZxudH19LmrrPjG2R1Mx80VJAMOCvCNx-A&s",
+    school: "Ryan International School, Delhi",
+    date: "Apr 2019 - Apr 2020",
+    grade: "86.8%",
+    desc: "I completed my class 10 education at Ryan International School, Delhi, where I studied Science with other subjects.",
+    degree: "CBSE(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Sundown Landing Page",
+    date: "Jan 2024",
+    description:
+      "A responsive landing page UI clone built using HTML, CSS, and JavaScript. This project aims to replicate a modern landing page layout commonly used for websites or web applications.",
+    image:
+      "https://github.com/user-attachments/assets/699e8471-a4a6-4a06-907a-5f79cf3b0806",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "GSAP",
+      "Locomotive",
+    ],
+    category: "landing page",
+    github: "https://github.com/VaibhavBahuguna1008/SundownWebsiteUIClone",
+
+  },
+  {
+    id: 9,
+    title: ".Store",
+    date: "Sept 2024 - Oct 2024",
+    description:
+      "This is a full-featured E - Commerce application built with the MERN (MongoDB, Express, React, Node.js) stack. The project includes essential features such as product browsing, filtering, Dashboard, Cart management, and User Authentication.",
+    image:
+      "https://github.com/user-attachments/assets/1b8a1887-1569-4a9e-af96-0f1c9c59c144",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "Axios",
+    ],
+    category: "web app",
+    github: "https://github.com/VaibhavBahuguna1008/.Store",
+
+  },
+  {
+    id: 10,
+    title: "Ochi Design Website",
+    date: "Nov 2024",
+    description:
+      "A pixel-perfect clone of Ochi Design's website, featuring smooth animations, minimalist design, and interactive elements. This project recreates the unique aesthetic and functionality of the award-winning Ochi design studio website.",
+    image:
+      "https://github.com/user-attachments/assets/9e39404e-6805-4e6a-8e23-c718ab3f1b21",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React Js",
+      "Tailwind",
+      "Framer Motion",
+      "Locomotive",
+    ],
+    category: "web app",
+    github: "https://github.com/VaibhavBahuguna1008/OchiDesignWebsite",
+
+  },
+  {
+    id: 11,
+    title: "Zentry Website",
+    date: "Oct 2024 - Nov 2024",
+    description:
+      "A visually stunning Gaming website inspired by Zentry, featuring scroll-triggered animations, sleek geometric transitions, and immersive video storytelling. Focus on delivering a modern, luxurious feel with an engaging UI/UX design and smooth responsiveness.",
+    image:
+      "https://github.com/user-attachments/assets/ab1d1cc4-aef2-46a4-ad4f-3ef89bb3a657",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React Js",
+      "Tailwind",
+      "Framer Motion",
+      "Locomotive",
+    ],
+    category: "web app",
+    github: "https://github.com/VaibhavBahuguna1008/ZentryWebsite",
+
+  },
+  {
+    id: 13,
+    title: "Rejouice - Venture Agency Website",
+    date: "Oct 2024 - Nov 2024",
+    description:
+      "Rejouice is a modern, feature-rich website designed to enhance user experiences through seamless navigation, appealing visuals, and functionality. This README provides a comprehensive overview of the project.",
+    image:
+      "https://github.com/user-attachments/assets/da71aadb-7646-4b3e-8b01-ab3cea24d244",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "ScrollTrigger",
+      "Swiper",
+      "Locomotive",
+    ],
+    category: "landing page",
+    github: "https://github.com/VaibhavBahuguna1008/RejouiceWebsite",
+
+  },
+  {
+    id: 12,
+    title: "3D Website Landing Page",
+    date: "October 2024",
+    description:
+      "This is a 3D interactive landing page built using Three.js, a JavaScript library for creating 3D graphics in the web browser. The landing page features animations and visual effects to create an engaging user experience.",
+    image:
+      "https://github.com/user-attachments/assets/3cba5754-300a-430b-9cb2-4bb826a3cdcc",
+    tags: [
+      "Three.js",
+      "JavaScript",
+      "HTML",
+      "CSS",
+    ],
+    category: "landing page",
+    github: "https://github.com/VaibhavBahuguna1008/3DWebsite",
+  },
+
+  {
+    id: 13,
+    title: "Real-Time Location Tracker",
+    date: "August 2024",
+    description:
+      "A real-time location tracker that displays a person's location on a map using Socket.io, HTML, and CSS. This project demonstrates the use of WebSockets for real-time communication, enabling dynamic updates of location data.",
+    image:
+      "https://github.com/user-attachments/assets/540663c8-b9b3-419b-8290-1cee59040e68",
+    tags: [
+      "EJS", 
+      "JavaScript", 
+      "CSS", 
+      "Web Sockets",
+      "Socket.io",
+    ],
+    category: "web app",
+    github: "https://github.com/VaibhavBahuguna1008/RealTimeTracker",
+  },
+  {
+    id: 14,
+    title: "Ephimeral Equilibrium - Creative Art Gallery",
+    date: "August 2024",
+    description:
+      "Ephimeral Equilibrium is a visually captivating and interactive website designed to showcase a modern art gallery. The site emphasizes creativity, elegance, and user engagement through dynamic visuals using SheryJS and advanced CSS techniques",
+    image:
+      "https://github.com/user-attachments/assets/f8477bb1-a8f6-438c-8648-5f2107570e2a",
+    tags: [
+      "EJS", 
+      "JavaScript", 
+      "CSS", 
+      "ScrollTrigger",
+      "Locomotive",
+      "SheryJS",
+    ],
+    category: "landing page",
+    github: "https://github.com/VaibhavBahuguna1008/EphemeralEquilibriumUIClone",
+  },
+
+  {
+    id: 15,
+    title: "Todo Web App",
+    date: "Jun 2024",
+    description:
+      " A simple todo application built with HTML, CSS, and JavaScript that utilizes local storage to save and manage tasks. This app allows users to add, edit, delete, and mark tasks as complete, providing a straightforward and user-friendly experience.",
+    image:
+      "https://github.com/user-attachments/assets/355d6365-477e-489f-a7d9-d2cf842ea7c0",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Local Storage",
+    ],
+    category: "web app",
+    github: "https://github.com/VaibhavBahuguna1008/ToDoApp?tab=readme-ov-file",
+
+  },
+  {
+    id: 16,
+    title: "WORKS UI Clone",
+    date: "July 2024",
+    description:
+      "A modern and responsive landing page UI clone built using HTML, CSS, and JavaScript and other libraries like GSAP for smooth animations and Locomotive for smooth scrolling ehus providing great user expreience.",
+    image:
+      "https://github.com/user-attachments/assets/6dde4958-f84f-4e13-818e-3f2c38c1cf57",
+    tags: ["HTML", "CSS", "JavaScript", "GSAP", "Locomotive"],
+    category: "landing page",
+    github: "https://github.com/VaibhavBahuguna1008/WorksUIClone",
+   
+  },
+  {
+    id: 17,
+    title: "CYBERFICTION Website",
+    date: "July 2024",
+    description:
+      "A futuristic website built using HTML, CSS, and JavaScript, featuring dynamic visuals powered by the HTML5 <canvas> element. This project combines cyberpunk aesthetics with modern web technologies to create an engaging and immersive user experience.",
+    image:
+      "https://github.com/user-attachments/assets/10f2934e-5324-48f6-b619-9e9cfe97d079",
+    tags: ["HTML", "CSS", "JavaScript", "Canvas", "Locomotive"],
+    category: "landing page",
+    github: "https://github.com/VaibhavBahuguna1008/CyberFictionWebsite",
+   
+  },
+  {
+    id: 18,
+    title: "Duo Studio - A Creative Digital Agency Website",
+    date: "August 2024",
+    description:
+      "Duo Studio is a visually engaging website built for a creative digital agency, showcasing its services and portfolio. This project was developed using HTML, CSS, and JavaScript, with additional libraries to enhance animations, scrolling effects, and user interactions.",
+    image:
+      "https://github.com/user-attachments/assets/77f210e0-5b71-44e1-9b4f-b944b8bb7e80",
+    tags: ["HTML", "CSS", "JavaScript", "GSAP", "Locomotive"],
+    category: "landing page",
+    github: "https://github.com/VaibhavBahuguna1008/DuoWebsite",
+   
+  },
+
+  
+];
+
